e25107cb028a1a94ae4a32f0930e1666ef16cbcf
GabrielRM <gabrielrm@MacBook-Air-de-Gabriel.local>
2018-01-11 14:33:45 -0200

    Arquivo de configuração

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+	/**
+	 * Configuração geral
+	 */
+
+	// URL da home
+	define( 'HOME_URI', 'http://localhost/clinica' );
+
+	// Nome do host da base de dados
+	define( 'HOSTNAME', 'localhost' );
+
+	// Nome do DB
+	define( 'DB_NAME', 'fconsulta' );
+
+	// Usuário do DB
+	define( 'DB_USER', 'root' );
+
+	// Senha do DB
+	define( 'DB_PASSWORD', '' );
+
+	// Charset da conexão PDO
+	define( 'DB_CHARSET', 'utf8' );
+
+?>
