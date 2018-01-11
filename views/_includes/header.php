5daeec0f77a764c57e0a720c1588dfa7b802148a
GabrielRM <gabrielrm@MacBook-Air-de-Gabriel.local>
2018-01-11 14:41:37 -0200

    Header/Menu/Footer/Style

---
+++ views/_includes/header.php
@@ -0,0 +1,18 @@
+<?php include "../config.php" ?>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="/clinica/views/_css/style.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<title><?php echo $title; ?></title>
+</head>
+<body>
+	<div class="main">
+		<div class="divPrincipalHeader">
+			<div class="marginLeft">
+				<div class="textoHeader divTitulo">
+					<strong>Dr. Agenda</strong>
+				</div>
+			</div>
+		</div>		
---
+++ views/_includes/menu.php
@@ -0,0 +1,8 @@
+	<nav class="menu clearfix">
+		<ul>
+			<li><a href="<?php echo HOME_URI;?>/">Home</a></li>
+			<li><a href="<?php echo HOME_URI;?>/medicos/">Médicos</a></li>
+			<li><a href="<?php echo HOME_URI;?>/clientes/">Clientes</a></li>
+			<li><a href="<?php echo HOME_URI;?>/consultas/">Consultas</a></li>
+		</ul>
+	</nav>
