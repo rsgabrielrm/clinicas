901241c88407eca11d5fdfa597b3bb89b15875d5
GabrielRM <gabrielrm@MacBook-Air-de-Gabriel.local>
2018-01-11 14:40:18 -0200

    View principal

---
+++ home/index.php
@@ -0,0 +1,11 @@
+<?php 
+	$title = 'Home';
+	include '../views/_includes/header.php';
+	include '../views/_includes/menu.php';
+?>
+<div class="marginLeft">
+	<strong>Sistema simples criado para clínicas...</strong>
+</div>
+<?php 
+	include '../views/_includes/footer.php';
+?>
